727a3ebc482a92d2b8b83c5db509e4a6373ea127
Tanishq Patel <tanishqpatel650@gmail.com>
2026-08-14 18:49:24 +0530

    Time: 3 ms (94.78%), Space: 44.3 MB (27.77%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,49 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-
         List<List<Integer>> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> tm = new TreeMap<>();
+        helper(tm, root, 0, 0);
 
-        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> tm = new TreeMap<>();
-
-        helper(root, tm, 0, 0);
-
-        for(Map.Entry<Integer,TreeMap<Integer,PriorityQueue<Integer>>> col : tm.entrySet()){
-
+        for (TreeMap<Integer, PriorityQueue<Integer>> outer : tm.values()) {
             List<Integer> al = new ArrayList<>();
-
-            for(Map.Entry<Integer,PriorityQueue<Integer>> row : col.getValue().entrySet()){
-
-                PriorityQueue<Integer> q = row.getValue();
-
-                while(!q.isEmpty()){
-                    al.add(q.poll());
+            for (PriorityQueue<Integer> inner : outer.values()) {
+                while (!inner.isEmpty()) {
+                    al.add(inner.poll());
                 }
             }
-
             ans.add(al);
         }
-
         return ans;
+
     }
 
-
-    public void helper(TreeNode root,
-                       TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> tm,
-                       int col,
-                       int row){
-
-        if(root == null)
+    public void helper(TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> tm, TreeNode root, int col, int row) {
+        if (root == null)
             return;
-
         tm.putIfAbsent(col, new TreeMap<>());
-
         tm.get(col).putIfAbsent(row, new PriorityQueue<>());
-
         tm.get(col).get(row).add(root.val);
-
-
-        helper(root.left, tm, col-1, row+1);
-
-        helper(root.right, tm, col+1, row+1);
+        helper(tm, root.left, col - 1, row + 1);
+        helper(tm, root.right, col + 1, row + 1);
     }
 }
