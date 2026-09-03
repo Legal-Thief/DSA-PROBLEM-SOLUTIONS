a034f6cfe41d515fa19098fcbb40d1cc105f8ce8
Tanishq Patel <tanishqpatel650@gmail.com>
2026-09-03 18:08:46 +0530

    Time: 65 ms (88.65%), Space: 48.1 MB (54.12%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,38 +1,64 @@
 class Solution {
-    class Pair{
+    class Pair {
         String word;
         int cnt;
-        Pair(String word,int cnt){
-            this.word= word;
-            this.cnt= cnt;
+
+        Pair(String word, int cnt) {
+            this.word = word;
+            this.cnt = cnt;
         }
     }
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<Pair> q =new LinkedList<>();
-        Set<String> st =new HashSet<>();
-        for(String word:wordList){
-            st.add(word);
+
+        Set<String> st = new HashSet<>(wordList);
+
+        if (!st.contains(endWord)) {
+            return 0;
         }
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(beginWord, 1));
         st.remove(beginWord);
-        q.offer(new Pair(beginWord,1));
-        int maxCnt=0;
-        while(!q.isEmpty()){
-            Pair curr= q.poll();
-            String word= curr.word;
+
+        while (!q.isEmpty()) {
+
+            Pair curr = q.poll();
+
+            String word = curr.word;
             int cnt = curr.cnt;
-            if(endWord.equals(word)) return cnt;
-            for(int i=0;i<word.length();i++){
-                for(char ch='a';ch<='z';ch++){
-                    char[] chWord=word.toCharArray();
-                    chWord[i]=ch;
-                    String newWord = new String(chWord);
-                    if(st.contains(newWord)){
+
+            if (word.equals(endWord)) {
+                return cnt;
+            }
+
+            char[] arr = word.toCharArray();
+
+            for (int i = 0; i < arr.length; i++) {
+
+                char original = arr[i];
+
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+
+                    if (ch == original) {
+                        continue;
+                    }
+
+                    arr[i] = ch;
+
+                    String newWord = new String(arr);
+
+                    if (st.contains(newWord)) {
                         st.remove(newWord);
-                        q.offer(new Pair(newWord,cnt+1));
+                        q.offer(new Pair(newWord, cnt + 1));
                     }
                 }
+
+                arr[i] = original;
             }
         }
+
         return 0;
     }
 }
